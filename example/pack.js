0ff32be4b3ee89b1b3975de5335df713afc424db
James Halliday <mail@substack.net>
2014-09-08 10:45:34 +0100

    works

--- example/pack.js
+++ example/pack.js
@@ -2,5 +2,6 @@ var spawn = require('child_process').spawn;
 var pack = require('../').pack;
 
 var cmd = process.argv.slice(2).join(' ');
-var ps = process.stdin.pipe(spawn(cmd));
+var ps = spawn(cmd);
+process.stdin.pipe(ps.stdin);
 pack(ps).pipe(process.stdout);
