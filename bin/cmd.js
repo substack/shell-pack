3a8a616d730dc2a52b29278d778b20fd4588c98d
James Halliday <mail@substack.net>
2014-09-08 20:01:14 +0100

    usage

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
 var minimist = require('minimist');
 var spawn = require('child_process').spawn;
 var parseShell = require('shell-quote').parse;
@@ -9,19 +10,24 @@ var pack = require('../').pack;
 var unpack = require('../').unpack;
 
 var argv = minimist(process.argv.slice(2), {
-    alias: { u: 'unpack', 1: 'stdout', 2: 'stderr' },
+    alias: { u: 'unpack', 1: 'stdout', 2: 'stderr', h: 'help' },
     'default': { stdout: 'green', stderr: 'red' },
     string: [ 'c' ],
-    'boolean': [ 'i' ]
+    'boolean': [ 'i', 'u' ]
 });
+if (argv.help) {
+    return fs.createReadStream(__dirname + '/usage.txt')
+        .pipe(process.stdout)
+    ;
+}
 
-if (argv.unpack) {
+var cmd = argv.c ? parseShell(argv.c) : argv._;
+if (cmd.length === 0 || argv.unpack) {
     var ps = process.stdin.pipe(unpack())
     ps.stdout.pipe(colorize(argv.stdout)).pipe(process.stdout);
     ps.stderr.pipe(colorize(argv.stderr)).pipe(process.stdout);
 }
 else {
-    var cmd = defined(argv.c, process.argv._.join(' '));
     var args = parseShell(cmd);
     var ps = spawn(args[0], args.slice(1));
 
